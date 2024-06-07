69f0b8f5e2089d15ef00b666652237e91e4b7ec0
Happollyon <36013973+Happollyon@users.noreply.github.com>
2024-06-07 15:02:15 +0000

    added jsdocs

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // https://www.youtube.com/watch?v=T-Pum2TraX4&ab_channel=JonathanSanchez
+// npm run docs
 import express from 'express';
 const app = express();
-
 import {createUser,selectAllUsers} from './db.js';
+
+/**
+ * @module backend
+ * @description This is the backend module
+ * @requires express
+ * @requires db
+ * @author Fagner Nunes
+ * @version 1.0
+ */
+
+
+/**
+ * @function createUser
+ * @description This function creates a new user
+ * @param {string} name - The name of the user
+ * @param {string} email - The email of the user
+ * @param {string} password - The password of the user
+ * @param {string} avatar - The avatar of the user
+ * @returns {Promise<void>}
+ */
+
 
 app.get('/create-user', (req, res) => {
     createUser('John Doe', 'john.doe@example.com', 'securePassword123', 'http://example.com/avatar.jpg');
 });
 
+
+/**
+ * @function selectAllUsers
+ * @description This function selects all users
+ * @returns {Promise<void>}
+ 
+ */
 app.get('/select-all-users', async (req, res) => {
   const users = await selectAllUsers();
   res.json({users});
